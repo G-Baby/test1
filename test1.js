8e31f51e83ca6ef6d3e43cfc3fb023eb8ce988cb
G-Baby <gpw10097@saintjoe.edu>
2015-02-18 13:17:24 -0500

    the test practicum

---
+++ test1.js
@@ -0,0 +1,8 @@
+//The first number 6 for the practicum
+var Ages = [];
+//input the ages of the people in your group into the array
+var GroupAge
+for (var Ages i=0; i<10; i++){
+	GroupAge += +Ages[i];
+}
+//will give you the total
